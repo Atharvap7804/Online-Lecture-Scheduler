2dd902da9ba180c7c6a9439c35bb56cbb00ea3c8
Atharvap7804 <atharvapawar53@gmail.com>
2026-09-24 14:55:37 +0530

    frontend changes

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://online-lecture-scheduler-yc31.onrender.com/api',
 });
 
 // For Admin
